5972b7db1dfabb9707bd2a235809e1393665fdfa
hexsorcerer <hexsorcerer03e9@gmail.com>
2020-11-18 20:55:04 -0500

    Add notes module
    Added a new module called notes, which provides a function that prints a string. This is then used to modify the current notes.txt file.
    
    This change represents the second challenge in the notes-app for the udemy course I'm following.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 const fs = require('fs')
+const getNotes = require('./notes.js')
 
 fs.writeFileSync('notes.txt', 'This file was created by Node.js!')
+fs.appendFileSync('notes.txt', '\n')
+fs.appendFileSync('notes.txt', getNotes())
