101ae067f92476d83600c87dc3b997c473f206ef
hexsorcerer <hexsorcerer03e9@gmail.com>
2020-11-19 21:10:57 -0500

    Write colored output to stdout
    After running the application, it now prints a success message to
    stdout with a green background.
    
    Note that you must enter the container with exec -it to see it, as
    it will not be printed to stdout on the host when run with
    docker-compose.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 fs.writeFileSync('notes.txt', 'This file was created by Node.js!')
@@ -7,3 +8,6 @@ fs.appendFileSync('notes.txt', '\n')
 fs.appendFileSync('notes.txt', getNotes())
 fs.appendFileSync('notes.txt', '\n')
 fs.appendFileSync('notes.txt', 'Valid URL: ' + validator.isURL('https://github.com/hexsorcerer'))
+fs.appendFileSync('notes.txt', '\n')
+
+console.log(chalk.black.bgGreen('Success!'))
