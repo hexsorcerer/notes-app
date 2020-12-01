3f08baff2616082647dd87d7471f52122a8c871a
hexsorcerer <hexsorcerer03e9@gmail.com>
2020-11-30 20:55:33 -0500

    Add real notes command
    Previously, the add notes command was just a placeholder. It now
    stores real notes, in a real file, with real JSON! It uses the
    functionality that was added in the notes module to do this.
    
    Other commands inc.

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
+const notes = require('./notes.js')
 
 yargs(hideBin(process.argv))
 .command({
     command: 'add <title> <note>',
     description: 'Add a new note',
-    handler: function (argv) {
-        fs.writeFileSync(argv.title, argv.note + '\n')
+    handler: (argv) => {
+        notes.addNote(argv.title, argv.note)
     }
 })
 .command({
